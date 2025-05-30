9ab74c9acc700a443bd7581e7af132f82d301973
taminur <taminur@live.com>
2025-05-30 17:49:23 +0600

    app.py modified with API keys

--- app.py
+++ app.py
@@ -1,30 +1,51 @@
-from flask import Flask, jsonify, send_file
+from flask import Flask, jsonify, send_file, request, abort, Response
 import os
 from scraper import collect_from_dsebd
 
 app = Flask(__name__)
 
-DATA_CSV = "latest_stock_data.csv"
+API_KEY = os.environ.get("API_KEY", "oLRg35Z-0i2wX5-nEN9hw7xd0WIdap_S6ZvuVQZPoaw")
 
-@app.route("/scrape", methods=["GET"])
+def require_api_key():
+    key = request.args.get("key") or request.headers.get("x-api-key")
+    if key != API_KEY:
+        abort(401, description="Unauthorized: Invalid or missing API key")
+
+@app.route("/scrape", methods=["POST"])
 def scrape():
+    require_api_key()
+    fmt = request.json.get("format", "json")  # default = json
+
     try:
         df = collect_from_dsebd()
-        # Save as CSV for download route
-        df.to_csv(DATA_CSV, index=False)
-        return jsonify({"success": True, "rows": len(df), "columns": list(df.columns), "data":df.to_json()})
+        
+        if fmt == "csv":
+            csv_data = df.to_csv(index=False)
+            return Response(
+                csv_data,
+                mimetype="text/csv",
+            )
+        else:
+            print('json will be returned')
+            json_data = df.to_json(orient="records")
+            return Response(
+                json_data,
+                mimetype="application/json",
+            )
     except Exception as e:
         return jsonify({"success": False, "error": str(e)})
 
-@app.route("/download", methods=["GET"])
+@app.route("/download", methods=["POST"])
 def download():
-    if not os.path.exists(DATA_CSV):
-        return jsonify({"success": False, "error": "No CSV file found. Please run /scrape first."})
-    return send_file(DATA_CSV, as_attachment=True)
+    require_api_key()
+    df = collect_from_dsebd()
+    data_csv = df.to_csv(index=False)
+    return send_file(data_csv, as_attachment=True)
 
 @app.route("/")
 def home():
-    return "<h3>DSE Scraper API</h3><ul><li><a href='/scrape'>/scrape</a> - fetch live data</li><li><a href='/download'>/download</a> - download CSV</li></ul>"
+    return ("<h3>DSE Scraper API</h3>"
+            )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
